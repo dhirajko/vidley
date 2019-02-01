271ffe7e3189ffe1d0c26c8f88b98285475c2db4
dhirajko <dhirajkoirala2013@gmail.com>
2019-02-01 02:14:12 +0200

    rental api debuged and inalized

--- router/rental.js
+++ router/rental.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const { Customer } = require("../models/customer");
 const { Movie } = require('../models/movie');
-const {Rental,rentalValidator} =require('../models/rental')
-const fawn=require('fawn');
+const { Rental, rentalValidator } = require('../models/rental')
+const fawn = require('fawn');
 
 
 fawn.init(mongoose);
@@ -34,7 +34,7 @@ router.post('/', async (req, res) => {
     const movie = await Movie.findById(req.body.movieId);
     if (!movie) return res.status(400).send(' The  movie with the given id is not found')
 
-  
+
 
     if (movie.numberInStock === 0) return res.send(' Movie not sufficient to rent')
 
@@ -48,71 +48,78 @@ router.post('/', async (req, res) => {
         movie: {
             _id: movie._id,
             title: movie.title,
-            dailyRentalRate: movie.dailyRentalRate 
-        }  
+            dailyRentalRate: movie.dailyRentalRate
+        }
     })
 
-    try{new fawn.Task()
+    try {
+        new fawn.Task()
         .save('rentals', rental)
-        .update('movies',{_id: movie._id},{
-            $inc: { numberInStock: -1}
+        .update('movies', { _id: movie._id }, {
+            $inc: { numberInStock: -1 }
         })
         .run()
         res.send(rental);
-    }catch(ex){
+    } catch (ex) {
         res.status(500).send('something failed')
     }
-    
+
 })
 
 router.put('/:id', async (req, res) => {
 
     const { error } = rentalValidator(req.body);
-    if (error) return res.status(400).send(error.details[0].message)   
+    if (error) return res.status(400).send(error.details[0].message)
 
-    
+
     const customer = await Customer.findById(req.body.customerId);
     if (!customer) return res.status(400).send('Customer of this id is not found');
 
     const movie = await Movie.findById(req.body.movieId);
-    if (!movie) return res.status(400).send(' The  movie with the given id is not found')     
-           
+    if (!movie) return res.status(400).send(' The  movie with the given id is not found')
+
 
     if (movie.numberInStock === 0) return res.send(' Movie not sufficient to rent')
 
-    const rental= await Rental.findById(req.params.id);
-    if(!rental) return res.status(400).send(" Movie of this id is not found")
-    
-    const prevMovie=await Movie.findById(rental.movie._id)
-    
-    
+    const rental = await Rental.findById(req.params.id);
+    if (!rental) return res.status(400).send(" Movie of this id is not found")
 
+    const prevMovie = await Movie.findById(rental.movie._id)
 
-    rental.customer={
+    rental.customer = {
         isGold: customer.isGold,
         _id: customer._id,
         name: customer.name,
         phone: customer.phone
     }
 
-    rental.movie={
+    rental.movie = {
         _id: movie.id,
         title: movie.title,
         dailyRentalRate: movie.dailyRentalRate
     }
-    
+
     await rental.save();
-    
+
     movie.numberInStock--;
     await movie.save();
 
     prevMovie.numberInStock++;
     prevMovie.save();
-      
-    
+
+
     res.send(rental)
-     
-    
+
+
+})
+
+router.delete('/:id', async (req, res) => {
+
+    const rental = await Rental.findByIdAndDelete(req.params.id);
+    if (!rental) return res.status(400).send(" Movie of this id is not found")
+
+    res.send(rental)
+
 })
 
 
@@ -121,4 +128,4 @@ router.put('/:id', async (req, res) => {
 
 
 
-module.exports=router;
+module.exports = router;
